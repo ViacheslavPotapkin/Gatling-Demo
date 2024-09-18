ab5e1f1c079a8fa3ee971b3d768e82cc83874073
viacheslavpotapkin <potapkin.vi@gmail.com>
2024-09-18 07:35:47 +0300

    fix

--- src/test/java/api/categoriesApi/CategoryApi.java
+++ src/test/java/api/categoriesApi/CategoryApi.java
@@ -10,26 +10,27 @@ import static io.gatling.javaapi.http.HttpDsl.status;
 public class CategoryApi {
 
     public static ChainBuilder getCategories = exec(
-            http("Get api.category")
-                    .get("/api/api.category")
+            http("Get category")
+                    .get("/api/category")
                     .check(status().is(200))
                     .check(jsonPath("$[0].name").is("For Him"))
     );
 
     public static ChainBuilder getCategoryById = exec(
-            http("Get api.category by id")
-                    .get("/api/product?api.category=7")
+            http("Get category by id")
+                    .get("/api/product?category=7")
                     .check(status().is(200))
     );
 
-    public static ChainBuilder updateCategory = exec(http("Update api.category")
-            .put("/api/api.category/7")
-            .headers(authorization)
-            .body(StringBody("""
-                    {
-                        "name": "Everyone"
-                    }
-                    """))
-            .check(jsonPath("$.name").is("Everyone"))
+    public static ChainBuilder updateCategory = exec(
+            http("Update category")
+                    .put("/api/category/7")
+                    .headers(authorization)
+                    .body(StringBody("""
+                            {
+                                "name": "Everyone"
+                            }
+                            """))
+                    .check(jsonPath("$.name").is("Everyone"))
     );
 }
